40d65eae4cb2f031adc884c8454841de97c4436c
TranBaKhoa1999 <ohwhynotmee1999@gmail.com>
2020-06-06 12:56:50 +0700

    Chơi Thử Đi

---
+++ Assets/Scripts/Controller Script/LevelMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BackButtonOnClick(){
+        Application.LoadLevel("MainMenu");
+    }
+    public void PlayLevel1Onlick(){
+        Application.LoadLevel("Level1");
+    }
+    public void PlayLevel2Onlick(){
+        Application.LoadLevel("Level2");
+    }
+}
