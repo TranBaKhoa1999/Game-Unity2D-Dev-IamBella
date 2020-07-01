6105126bee828ace71fee05ac068477169e3ed3b
TranBaKhoa1999 <ohwhynotmee1999@gmail.com>
2020-07-02 00:48:17 +0700

    player:  L : use skill  Enter : normal attack

--- Assets/Scripts/Player Script/Moving.cs
+++ Assets/Scripts/Player Script/Moving.cs
@@ -12,9 +12,12 @@ public class Moving : MonoBehaviour
 
     private Rigidbody2D myBody;
     private Animator anim;
-
+    private bool shotable=true;
     public float minX,maxX;
     public Transform player;
+
+    [SerializeField]
+    public GameObject bullet;
     void Awake(){
         myBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
@@ -29,17 +32,24 @@ public class Moving : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-            PlayerMoveKeyboard();
-            if(anim.GetCurrentAnimatorStateInfo(0).IsName("PhysicalAttack") && 
-   anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-{
-    anim.SetBool("isPhysicalAttack", false);
-}
-if(anim.GetCurrentAnimatorStateInfo(0).IsName("MagicalAttack") && 
-   anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-{
-    anim.SetBool("isMagicalAttack", false);
-}
+        MagicalAttack();
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("PhysicalAttack") && // code sau khi hoàn thành đánh thường thì ko đánh lại lần nữa
+            anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                anim.SetBool("isPhysicalAttack", false);
+            }
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("MagicalAttack") &&  // code sau khi hoàn thành vận skill thì ko vận lại lần nữa
+        anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            anim.SetBool("isMagicalAttack", false);
+        }
+
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("MagicalAttack")){ // nếu đang vận skill magic thì ko cho di chuyển
+            transform.position = transform.position;
+        }
+        else{
+                    PlayerMoveKeyboard();
+        }
     }
     void PlayerMoveKeyboard(){
         float forceX = 0f;
@@ -97,6 +107,7 @@ if(anim.GetCurrentAnimatorStateInfo(0).IsName("MagicalAttack") &&
                 
                 MainMenuController.level2=true;
                 MainMenuController.SaveData();
+                shotable=true;
  // ------------------------------------------- End test -------------------------
             }
         }
@@ -105,14 +116,17 @@ if(anim.GetCurrentAnimatorStateInfo(0).IsName("MagicalAttack") &&
                 
             }
             else{
-                if(anim.GetBool("isMagicalAttack")==false && grounded)
-                anim.SetBool("isPhysicalAttack",true);
+                if(anim.GetBool("isMagicalAttack")==false && grounded){
+                    anim.SetBool("isPhysicalAttack",true);
+                }
             }
         }
-        else if(Input.GetKey (KeyCode.L)){
-            if(anim.GetBool("isPhysicalAttack")==false & grounded)
-            anim.SetBool("isMagicalAttack",true);
-        }
+        // else if(Input.GetKey (KeyCode.L)){
+        //     if(anim.GetBool("isPhysicalAttack")==false & grounded){
+        //         anim.SetBool("isMagicalAttack",true);
+        //         //MagicalAttack();
+        //     }
+        // }
         myBody.AddForce( new Vector2(forceX,forceY));
         //  limited move range
         Vector3 temp = transform.position;
@@ -130,4 +144,22 @@ if(anim.GetCurrentAnimatorStateInfo(0).IsName("MagicalAttack") &&
             grounded = true;
         }
     }
+
+    void MagicalAttack(){
+
+        if(Input.GetKey (KeyCode.L) && shotable){;
+            if(anim.GetBool("isPhysicalAttack")==false & grounded){
+                anim.SetBool("isMagicalAttack",true);
+                // BulletShow();
+                Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, 0 )));
+                shotable=false;
+            }
+        }
+    }
+    //      private IEnumerator BulletShow()
+    //  {        
+    //      //Wait for 14 secs.
+    //      yield return new WaitForSeconds(5);
+
+    //  }
 }
---
+++ Assets/Scripts/Skill Script/MagicalScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+    public Transform player;
+    private float side;
+    void Awake(){
+    }
+    void Start()
+    {
+        side = player.transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move(){
+        if(side<0){
+            transform.Translate(transform.right * speed * Time.smoothDeltaTime);
+        }
+        else{
+            transform.Translate(transform.right * speed * Time.smoothDeltaTime*-1);
+        }
+    }
+}
