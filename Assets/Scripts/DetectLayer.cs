0948fd2b7cab2d51658148fab957c9f2e9053969
Khoa <ohwhynotme1999@gmail.com>
2020-07-12 11:47:22 +0700

    fix xíu

--- Assets/Scripts/DetectLayer.cs
+++ Assets/Scripts/DetectLayer.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class DetectLayer : MonoBehaviour
 {
-    private Transform player;
-    public Transform playerpos;
-    private bool colli;
-    private int x;
     // Start is called before the first frame update
-    void Awake(){
-        player = GameObject.Find("Player").transform;
-    }
     void Start()
     {
         
@@ -20,15 +13,6 @@ public class DetectLayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // //RaycastHit2D hit;
-        // // Debug.Log(RaycastHit2D);
-        // colli = Physics2D.Linecast(player.position, playerpos.position,1 << LayerMask.NameToLayer("Temp"));
-        // if(colli){
-        //     gameObject.layer=LayerMask.NameToLayer("EnemyFollowRange");
-        // }
-        // else{
-        //     gameObject.layer=LayerMask.NameToLayer("EnemyMoveRange");
-        // }
     }
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag== "Player"){
